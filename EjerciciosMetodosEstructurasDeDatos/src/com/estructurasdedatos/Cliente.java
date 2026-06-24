7c253810febae6e75ebcca5872c93b62eaf5c20e
Arnaldo Andrés Barrios Mena <cyberarnaldo04@hotmail.com>
2026-06-24 18:18:57 -0500

    Javadoc en la clase Cliente.

--- EjerciciosMetodosEstructurasDeDatos/src/com/estructurasdedatos/Cliente.java
+++ EjerciciosMetodosEstructurasDeDatos/src/com/estructurasdedatos/Cliente.java
@@ -37,36 +37,59 @@ public class Cliente {
         return "Cliente{Id= " + id + " cedula= " + cedula + ", nombre='" + nombre + "', apellido='" + apellido + "'}";
     }
 
+    /**
+     * @return the id
+     */
     public int getId(){
         return id;
     }
 
+    /**
+     * @param id the id to set
+     */
     public void setId(int id){
         this.id = id;
     }
 
+    /**
+     * @return the cedula
+     */
     public String getCedula(){
         return cedula;
     }
 
+    /**
+     * @param cedula the cedula to set
+     */
     public void setCedula(String cedula){
         this.cedula = cedula;
     }
 
+    /**
+     * @return the nombre
+     */
     public String getNombre(){
         return nombre;
     }
 
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
 
+    /**
+     * @return the apellido
+     */
     public String getApellido(){
         return apellido;
     }
 
+    /**
+     * @param apellido the apellido to set
+     */
     public void setApellido(String apellido){
         this.apellido = apellido;
     }
-
 }
